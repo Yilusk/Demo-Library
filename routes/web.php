651d0cf7ea272ca21cae4270f651f35a5e0d94eb
¨Alexander <baeza.ae@hotmail.com>
2024-11-06 19:44:17 -0300

    Added routes for new home views

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,20 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
-use App\Models\Author;
-use App\Models\Book;
-use App\Models\Category;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/search',[HomeController::class,'searchView'])->name('home.search');
+Route::get('/show/{book}',[HomeController::class,'showDetail'])->name('home.show');
 
 Route::middleware('auth')->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
-    Route::get('/about', fn() => Inertia::render('About'))->name('about');
 
     Route::get('users', [UserController::class, 'index'])->name('users.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
